8a2479b14364b08afcc5f349e70d0d94d80eb599
Matias Mercado <166638575+Eseekaa@users.noreply.github.com>
2025-06-06 02:41:51 -0400

    Add files via upload

---
+++ vendedores-api-spring-boot/src/main/java/com/api/vendedores_api_spring_boot/VendedoresApiSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.api.vendedores_api_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VendedoresApiSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VendedoresApiSpringBootApplication.class, args);
+	}
+
+}
